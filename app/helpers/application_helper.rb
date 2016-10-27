2dc4571e34a07edb7094a4e0215ef64742b4859e
Stuart Marquis <marquis@columbia.edu>
2016-10-27 15:04:38 -0400

    Update to GeoBlacklight 1.2.0, etc.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 
-  # For Columbia, ALL documents are downloadable via
-  # an authenticated Direct Download link.
+  # For Columbia, ALL documents are downloadable,
+  # if they have an authenticated Direct Download link.
   def document_downloadable?
     # document_available? && @document.downloadable?
-    return true
+    # return true
+    @document.direct_download.present?
   end
 
 end
