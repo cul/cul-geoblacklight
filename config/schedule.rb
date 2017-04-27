73f0a1d578769ca826686da6de7369da71037593
Stuart Marquis <marquis@columbia.edu>
2017-04-27 16:42:43 -0400

    tweak

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,25 @@
 # Use this file to easily define all of your cron jobs.
 # Learn more: http://github.com/javan/whenever
 
-# Load rails environment
-require File.expand_path('../config/environment', __dir__)
-
-# Set environment to current environment.
-set :environment, Rails.env
+# # Load rails environment
+# require File.expand_path('../config/environment', __dir__)
+# 
+# # Set environment to current environment.
+# set :environment, Rails.env
 
 # Give our jobs nice subject lines
-set :email_subject, "cron output"
-set :job_template, "/usr/local/bin/mailifoutput -s ':email_subject (:environment)' -- /bin/bash -l -c ':job'"
+set :subject, "cron output"
+set :to, Etc.getpwuid(Process.uid).name
+set :job_template, "/usr/local/bin/mailifoutput -s ':subject (@environment)' :to -- /bin/bash -l -c ':job'"
 
 
 # Run on every host - dev, test, prod
 every :monday, at: '1am' do
-  rake 'metadata:process', email_subject: 'GeoBL metadata:process'
+  rake 'metadata:process', subject: 'GeoBL metadata:process output'
 end
 
-every :thursday, at: '4:25pm' do
-  rake 'metadata:process', email_subject: 'GeoBL metadata:process'
+every :thursday, at: '4:45pm' do
+  rake 'metadata:process', subject: 'GeoBL metadata:process output'
 end
 
 
