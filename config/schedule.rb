b3839d31eb474f2bbb0f4da71c7bf77748d05897
Stuart Marquis <marquis@columbia.edu>
2017-04-27 17:02:28 -0400

    fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,25 +1,25 @@
 # Use this file to easily define all of your cron jobs.
 # Learn more: http://github.com/javan/whenever
 
-# # Load rails environment
-# require File.expand_path('../config/environment', __dir__)
-# 
-# # Set environment to current environment.
-# set :environment, Rails.env
+# Load rails environment
+require File.expand_path('../config/environment', __dir__)
+
+# Set environment to current environment.
+set :environment, Rails.env
 
 # Give our jobs nice subject lines
-set :subject, "cron output"
-set :to, 'litoserv'
-set :job_template, "/usr/local/bin/mailifoutput -s ':subject (@environment)' :to -- /bin/bash -l -c ':job'"
+set :subject, 'cron output'
+set :recipient, 'litoserv'
+set :job_template, "/usr/local/bin/mailifoutput -s ':subject (:environment)' :recipient -- /bin/bash -l -c ':job'"
 
 
 # Run on every host - dev, test, prod
 every :monday, at: '1am' do
-  rake 'metadata:process', subject: 'GeoBL metadata:process output'
+  rake 'metadata:process', subject: 'GeoData metadata:process output'
 end
 
-every :thursday, at: '4:55pm' do
-  rake 'metadata:process', subject: 'GeoBL metadata:process output'
+every :thursday, at: '5:05pm' do
+  rake 'metadata:process', subject: 'GeoData metadata:process output'
 end
 
 
