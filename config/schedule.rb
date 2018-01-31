e1a9318b1682135d9534a102d69c1e8646aa12b9
Stuart Marquis <stuartmarquis@gmail.com>
2018-01-31 14:56:15 -0500

    Try again setting PATH for whenever cron rake jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,14 +12,17 @@ set :subject, 'cron output'
 set :recipient, 'geodata@library.columbia.edu'
 set :job_template, "/usr/local/bin/mailifoutput -s ':subject (:environment)' :recipient /bin/bash -l -c ':job'"
 
+env 'PATH', '/bin:/usr/bin:/home/litoserv/.rvm/wrappers/geoblacklight'
+
+# Rake jobs should use the GeoBlacklight verion of Ruby, not the rvm default
+job_type :rake,    "cd /opt/passenger/lito/geoblacklight_dev/current && :environment_variable=:environment ~/.rvm/wrappers/geoblacklight/bundle exec rake :task --silent :output"
+
 
 # Run on every host - dev, test, prod
 every :day, at: '1am' do
   rake 'metadata:process', subject: 'GeoData metadata:process output'
 end
 
-# Rake jobs should use the GeoBlacklight verion of Ruby, not the rvm default
-job_type :rake,    "cd :path && :environment_variable=:environment ~/.rvm/wrappers/geoblacklight/bundle exec rake :task --silent :output"
 
 
 # Examples of per-environment cron commands
