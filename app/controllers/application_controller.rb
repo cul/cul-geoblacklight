d57a7aab18ce33b8cd770ea43f95e93bffa71566
Stuart Marquis <stuartmarquis@gmail.com>
2018-10-03 13:38:06 -0400

    LIBSYS-1434 - http to https for web

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
     end
     
     # Full CAS logout + application logout page looks like this:
-    # http://cas.columbia.edu/cas/logout?service=https://helpdesk.cul.columbia.edu/welcome/logout
+    # https://cas.columbia.edu/cas/logout?service=https://helpdesk.cul.columbia.edu/welcome/logout
     cas_logout_url = 'https://' + cas_opts['host'] + cas_opts['logout_url']
     service = request.base_url
     after_sign_out_path = "#{cas_logout_url}?service=#{service}"
